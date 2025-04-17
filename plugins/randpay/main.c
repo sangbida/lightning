97cd8f5d1d1fd2269dfd2aab89409d2ef2f7350f
sangbida <sangbida@squareup.com>
2025-04-17 16:37:42 +1000

    return a status and a message

--- plugins/randpay/main.c
+++ plugins/randpay/main.c
@@ -5,12 +5,13 @@
 #include <common/json_stream.h>
 #include <plugins/libplugin.h>
 
+
 /* Command handler for the "hello" command */
 
 enum return_value {
-	RED,
-	YELLOW,
-	GREEN,
+    RED,
+    YELLOW,
+    GREEN,
 };
 
 static const char *enum_to_string(enum return_value val) {
@@ -30,44 +31,43 @@ static struct command_result *json_hello(struct command *cmd,
                                        const char *buf,
                                        const jsmntok_t *params)
 {
-    uint32_t *enum_val;
+    unsigned int *enum_val;
     const char *enum_str;
     const char *message;
     struct json_stream *response;
 
-    /* Log entry to the command handler */
+
     plugin_log(cmd->plugin, LOG_DBG, "Hello command received, parsing parameters...");
 
-    /* Parse parameters: optional "name" parameter */
+    /* Parse parameters */
     if (!param(cmd, buf, params,
               p_req("status_code", param_number, &enum_val),
-	       	  p_req("message", param_string, &message),
+              p_req("message", param_string, &message),
               NULL)) {
         plugin_log(cmd->plugin, LOG_BROKEN, "Parameter parsing failed!");
         return command_param_failed();
     }
 
+    if (*enum_val > GREEN) {
+        return command_fail(cmd, JSONRPC2_INVALID_PARAMS,
+                           "status_code must be between 0 and 2");
+    }
+
+
     enum_str = enum_to_string((enum return_value)*enum_val);
-
-    plugin_log(cmd->plugin, LOG_DBG, "Status code: %ld, Message: %s",
-               (long)enum_val, message);
-
+    plugin_log(cmd->plugin, LOG_DBG, "Status code: %u, Message: %s",
+               *enum_val, message);
 
     response = jsonrpc_stream_success(cmd);
 
     json_add_num(response, "status_code", *enum_val);
     json_add_string(response, "status", enum_str);
     json_add_string(response, "message", message);
-
-
     plugin_log(cmd->plugin, LOG_INFORM, "Returning status %s with message: %s",
                enum_str, message);
-
-    /* Return the response to the caller */
-    return command_success(cmd, response->jout);
+    return command_finished(cmd, response);
 }
 
-/* Array of commands we provide */
 static const struct plugin_command commands[] = {
     {
         "hello",           /* command name */
