5060e6ea82a5dc5732bcdcba96206dbc47ae18ba
sangbida <sangbida@squareup.com>
2025-04-17 13:41:21 +1000

    Add some more logging

--- plugins/randpay/main.c
+++ plugins/randpay/main.c
@@ -13,31 +13,53 @@ static struct command_result *json_hello(struct command *cmd,
     struct json_stream *response;
     const char *name;
     
+    /* Log entry to the command handler */
+    plugin_log(cmd->plugin, LOG_DBG, "Hello command received, parsing parameters...");
+
     /* Parse parameters: optional "name" parameter */
     if (!param(cmd, buf, params,
               p_opt("name", param_string, &name),
-              NULL))
+              NULL)) {
+        plugin_log(cmd->plugin, LOG_BROKEN, "Parameter parsing failed!");
         return command_param_failed();
+    }
 
     /* If no name provided, use "world" */
-    if (!name)
+    if (!name) {
+        plugin_log(cmd->plugin, LOG_DBG, "No name provided, using default 'world'");
         name = "world";
+    } else {
+        plugin_log(cmd->plugin, LOG_DBG, "Name parameter: %s", name);
+    }
+
+    /* Log that we're creating the response */
+    plugin_log(cmd->plugin, LOG_DBG, "Creating success response...");
 
     /* Create success response */
     response = jsonrpc_stream_success(cmd);
+    if (!response) {
+        plugin_log(cmd->plugin, LOG_BROKEN, "Failed to create JSON response stream!");
+        return command_fail(cmd, LIGHTNINGD, "Internal error: couldn't create response");
+    }
+
+    /* Add the greeting to the response */
+    plugin_log(cmd->plugin, LOG_DBG, "Adding greeting to response...");
     json_add_string(response, "greeting", tal_fmt(cmd, "Hello, %s!", name));
-    
+
+    /* Log before returning the response */
+    plugin_log(cmd->plugin, LOG_DBG, "Returning successful response with greeting");
+
+    /* Return the response to the caller */
     return command_success(cmd, response->jout);
 }
 
 /* Array of commands we provide */
 static const struct plugin_command commands[] = {
     {
-        "hello",           /* Command name */
-        json_hello,        /* Command handler */
-        "Say hello",       /* Command description */
-        "hello [name]",    /* Command usage */
-        false             /* Not a developer command */
+        "hello",           /* command name */
+        json_hello,        /* handler function */
+        NULL, NULL,        /* not deprecated: depr_start, depr_end = NULL */
+        false              /* dev_only: false */
     }
 };
 
@@ -46,7 +68,11 @@ static const char *init(struct command *cmd,
                        const char *buf UNUSED,
                        const jsmntok_t *config UNUSED)
 {
-    /* Nothing to initialize */
+    /* Log a message to confirm plugin initialization */
+    plugin_log(cmd->plugin, LOG_INFORM, "Hello plugin initialization started");
+    plugin_log(cmd->plugin, LOG_INFORM, "Plugin version: 1.0");
+    plugin_log(cmd->plugin, LOG_INFORM, "Registering 'hello' command");
+    plugin_log(cmd->plugin, LOG_INFORM, "Hello plugin initialization completed successfully!");
     return NULL;
 }
 
@@ -54,7 +80,7 @@ int main(int argc, char *argv[])
 {
     /* Set up locale for proper formatting */
     setup_locale();
-    
+
     /* Start plugin */
     plugin_main(argv,
                 init,                /* initialization function */
@@ -68,6 +94,6 @@ int main(int argc, char *argv[])
                 NULL, 0,           /* hooks */
                 NULL, 0,           /* notification topics */
                 NULL);             /* options */
-    
+
     return 0;
 }
